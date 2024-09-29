9c18b6c11ec6ddb309dfc6a4f9a87a2148d93807
Mankaistep <hhoang21vn@gmail.com>
2024-09-29 16:16:39 +0700

    update build_script

--- build_script.sh
+++ build_script.sh
@@ -8,7 +8,15 @@ mkdir -p temp_built
 
 # Copy standalone build to the temporary directory
 cp -R .next/standalone/* temp_built/
+
+# Create .next directory and copy required files
+mkdir -p temp_built/.next
 cp -R .next/static temp_built/.next/
+cp -R .next/standalone/.next/routes temp_built/.next/
+cp -R .next/standalone/.next/server temp_built/.next/
+cp -R .next/standalone/.next/build-manifest.json temp_built/.next/
+cp -R .next/standalone/.next/react-loadable-manifest.json temp_built/.next/
+
 cp package.json temp_built/
 
 # Copy public folder if it exists
@@ -21,8 +29,11 @@ echo "node_modules" > temp_built/.gitignore
 echo ".env" >> temp_built/.gitignore
 
 # Create a start script
-echo "#!/bin/bash" > temp_built/start.sh
-echo "PORT=\${PORT:-3000}" >> temp_built/start.sh
-echo "HOST=\${HOST:-localhost}" >> temp_built/start.sh
-echo "node server.js" >> temp_built/start.sh
+cat << EOF > temp_built/start.sh
+#!/bin/bash
+export NODE_ENV=production
+export PORT=\${PORT:-3000}
+export HOST=\${HOST:-0.0.0.0}
+node server.js
+EOF
 chmod +x temp_built/start.sh
