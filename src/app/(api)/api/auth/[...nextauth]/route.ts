e227d425df52118fe8c7716e14178158376b6e97
mankaistep <hhoang21vn@gmail.com>
2024-10-13 18:30:16 +0700

    fix authOptions export 2

--- src/app/(api)/api/auth/[...nextauth]/route.ts
+++ src/app/(api)/api/auth/[...nextauth]/route.ts
@@ -1,62 +1,6 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import type { NextAuthOptions } from "next-auth"
-import prisma from "@/lib/prisma"
-
-const authOptions: NextAuthOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-            authorization: {
-                params: {
-                    prompt: "select_account"
-                }
-            }
-        }),
-    ],
-    pages: {
-        signIn: "/auth",
-    },
-    callbacks: {
-        async signIn({ user, email }) {
-            if (!email?.verificationRequest) {
-                await prisma.user.upsert({
-                    where: {
-                        googleId: user.id
-                    },
-                    create: {
-                        name: user.name,
-                        email: user.email,
-                        image: user.image,
-                        googleId: user.id
-                    },
-                    update: {
-                        name: user.name,
-                        email: user.email,
-                        image: user.image
-                    }
-                })
-            }
-            return true
-        },
-        async jwt({ token, user }) {
-            if (user) {
-                token.id = user.id
-            }
-            return token
-        },
-        async session({ session, token }) {
-            if (session.user) {
-                // @ts-ignore
-                session.user.id = token.id as string
-            }
-            return session
-        }
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-}
+import { authOptions } from "@/lib/auth-utils";
 
 const handler = NextAuth(authOptions)
 
-export { handler as GET, handler as POST, authOptions }
+export { handler as GET, handler as POST }
--- src/lib/auth-utils.ts
+++ src/lib/auth-utils.ts
@@ -1,6 +1,8 @@
 import {NextRequest, NextResponse} from "next/server";
 import {getToken} from "next-auth/jwt";
 import prisma from "@/lib/prisma";
+import type {NextAuthOptions} from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 export async function getUser(request: NextRequest) {
     const token = await getToken({
@@ -18,4 +20,58 @@ export async function getUser(request: NextRequest) {
     return prisma.user.findFirst({
         where: {email: userEmail},
     });
+}
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            authorization: {
+                params: {
+                    prompt: "select_account"
+                }
+            }
+        }),
+    ],
+    pages: {
+        signIn: "/auth",
+    },
+    callbacks: {
+        async signIn({ user, email }) {
+            if (!email?.verificationRequest) {
+                await prisma.user.upsert({
+                    where: {
+                        googleId: user.id
+                    },
+                    create: {
+                        name: user.name,
+                        email: user.email,
+                        image: user.image,
+                        googleId: user.id
+                    },
+                    update: {
+                        name: user.name,
+                        email: user.email,
+                        image: user.image
+                    }
+                })
+            }
+            return true
+        },
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id
+            }
+            return token
+        },
+        async session({ session, token }) {
+            if (session.user) {
+                // @ts-ignore
+                session.user.id = token.id as string
+            }
+            return session
+        }
+    },
+    secret: process.env.NEXTAUTH_SECRET,
 }
